7167e7f2e8172d8dd664448de26988d4c936ef31
Berserk <dilmuradovid@gmail.com>
2024-12-07 17:56:27 +0500

    Figma-5

---
+++ main.js
@@ -0,0 +1,39 @@
+// Exercise(ChatGPT) --- 1 ---
+
+// let str = "JavaScriptIsEasy"
+// let res = str.slice(0,11)
+// let result = res.toLowerCase()
+// console.log(result);
+
+// Exercise(ChatGPT) --- 2 ---
+
+// let str = '   Hello Ann How Are You  '
+// let newStr = str.trim()
+// let arr = []
+// for (let i = 0; i < newStr.length; ++i) {
+// 	if (newStr[i] === ' ') {
+// 		arr.push(i)
+// 	}
+// }
+// console.log(arr);
+
+// Exercise(ChatGPT) --- 3 ---
+
+// let str = '   Hello Ann How Are You  '
+// let newStr = str.trim()
+// let res = newStr.split(' ')
+// console.log(res);
+
+// Exercise(ChatGPT) --- 4 ---
+
+// let str = 'apple123orange456banana879'
+// let allowed = 'abcdefghijklmnopqrstuvwxyz'
+// let res = ''
+// for (let i = 0; i < str.length; ++i) {
+// 	for (let j = 0; j < str.length; ++j) {
+// 		if (str[i] === allowed[j]) {
+// 			res += str[i]
+// 		}
+// 	}
+// }
+// console.log(res)
